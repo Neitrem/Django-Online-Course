14c78c6fc8c56f3e70bfb098676c4d365b74ff1e
Neitrem <a.v.gorshenin003@gmail.com>
2023-03-09 16:55:50 +0300

    some changes for pylint

--- authentication/models.py
+++ authentication/models.py
@@ -1,16 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-from courses.models import Course
-
 from simple_history.models import HistoricalRecords
+
+from courses.models import Course
 
 from web_project.validators import correct_string_length, correct_age_value
 from authentication.manager import UserManager
 
 # Create your models here.
 class User(AbstractBaseUser, PermissionsMixin):
-    
     TEACHER = 'Teacher'
     STUDENT = "Student"
     ROLE_CHOICES = [
@@ -22,7 +21,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(verbose_name='First name ', max_length=255)
     last_name = models.CharField(verbose_name='Last name ', max_length=255)
     age = models.IntegerField(verbose_name='User age ', blank=True, null=True)
-    user_role = models.CharField(verbose_name='User role ', max_length=255, choices=ROLE_CHOICES, default=STUDENT, null=True)
+    user_role = models.CharField(verbose_name='User role ', max_length=255, 
+                                choices=ROLE_CHOICES, default=STUDENT, null=True)
     
     is_active = models.BooleanField(verbose_name='Is active', default=False)
     is_staff = models.BooleanField(verbose_name='Is staff', default=False)
@@ -34,10 +34,10 @@ class User(AbstractBaseUser, PermissionsMixin):
     history = HistoricalRecords()
     
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'is_staff']
+    REQUIRED_FIELDS = ['first_name', 'last_name']
     
-    def __str__(self):
-        return self.email
+    def __str__(self) -> str:
+        return str(self.email)
     
     def clean(self):
         correct_string_length(self.first_name, 4)
@@ -55,30 +55,34 @@ class User(AbstractBaseUser, PermissionsMixin):
     
 
 class Student(models.Model):
-    user_id = models.OneToOneField(verbose_name='User email ', to=User, primary_key=True, on_delete=models.CASCADE)
+    user_id = models.OneToOneField(verbose_name='User email ', 
+                                    to=User, primary_key=True, on_delete=models.CASCADE)
     
-    courses_id = models.ManyToManyField(verbose_name='User courses ', to = Course, related_name = 'user_courses', blank=True)
+    courses_id = models.ManyToManyField(verbose_name='User courses ',
+                                    to = Course, related_name = 'user_courses', blank=True)
     
     history = HistoricalRecords()
     
     def __str__(self):
-        return self.user_id.email
+        return str(self.user_id.email)
     
     class Meta:
         verbose_name = 'Student'
         verbose_name_plural = 'Students'
     
 class Teacher(models.Model):
-    user_id = models.OneToOneField(verbose_name='User email ', to=User, primary_key=True, on_delete=models.CASCADE)
+    user_id = models.OneToOneField(verbose_name='User email ', 
+                                    to=User, primary_key=True, on_delete=models.CASCADE)
     
-    courses_id = models.ManyToManyField(verbose_name='Teacher courses ', to = Course, related_name = 'teacher_courses', blank=True)
+    courses_id = models.ManyToManyField(verbose_name='Teacher courses ', 
+                                    to = Course, related_name = 'teacher_courses', blank=True)
     
     work_age = models.IntegerField(verbose_name='Techer work age ',  null=True)
     
     history = HistoricalRecords()
     
     def __str__(self):
-        return self.user_id.email
+        return str(self.user_id.email)
     
     class Meta:
         verbose_name = 'Teacher'
